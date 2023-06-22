132d3bb9ff5b7aaefe204fc0f4c537a1c88a97df
sayv <savegawugah01@gmail.com>
2023-06-22 23:25:00 +0000

    Stack operator that does nothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Opcode that does nothing
+ * @stack: The stack or queue
+ * @line_number: Line number in file
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
