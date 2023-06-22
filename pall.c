a3bd36fe4d343da03fde981de083dcdbdcc894b3
sayv <savegawugah01@gmail.com>
2023-06-22 23:25:43 +0000

    Pall operator

---
+++ pall.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+
+/**
+ * op_pall - Print element to the stack
+ * @stack_t: The stack or queue
+ * @line_number: The line number
+ * Return: Nothing
+ */
+void op_pall(stact_t **stack, unsigned int line_number)
+{
+
